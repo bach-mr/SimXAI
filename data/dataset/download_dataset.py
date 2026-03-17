436a4b07f668fb83c77b9802079bd29451e04669
pikachu <nvbach92@gmail.com>
2026-03-17 08:50:02 +0100

    restructure

--- data/dataset/download_dataset.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset("stanfordnlp/imdb", split="test")
-
-df = ds.to_csv("imdb_test.csv", index=True)
--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model_loader import ModelLoader
-from .inference import TextGenerator
-from .config import ModelConfig, InferenceConfig
-
-__all__ = ['ModelLoader', 'TextGenerator', 'ModelConfig', 'InferenceConfig'] 
